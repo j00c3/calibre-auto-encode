ea37bafecc2afb03ac5b2a753e8c0f36d0560859
j00c3 <joohackjoo@gmail.com>
2022-12-23 18:23:58 -0500

    remove unnecessary handlers and add subprocess.run for on_created event

--- calibre_watchdog.py
+++ calibre_watchdog.py
@@ -1,30 +1,19 @@
-import time
+import time, subprocess
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 def on_created(event):
     print(f"{event.src_path} has been created.")
+    subprocess.run(["calibre-debug", "/root/modify_epub_plugin/commandline/me.py", "--", "--help"])
 
-def on_deleted(event):
-    print(f"{event.src_path} deleted.")
-
-def on_modified(event):
-    print(f"hey buddy, {event.src_path} has been modified")
-
-def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
-
-if __name__ == "__main__":
+def main():
     patterns = ["*.epub"]
     ignore_patterns = None
-    ignore_directories = False
+    ignore_directories = True
     case_sensitive = True
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
     my_event_handler.on_created = on_created
-    my_event_handler.on_deleted = on_deleted
-    my_event_handler.on_modified = on_modified
-    my_event_handler.on_moved = on_moved
 
     path = "."
     go_recursively = True
@@ -38,3 +27,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         my_observer.stop()
         my_observer.join()
+
+if __name__ == "__main__":
+    main()
