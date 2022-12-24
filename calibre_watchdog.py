0200c3788e1b3a8181d1359a48f652bebc45d5d5
j00c3 <joohackjoo@gmail.com>
2022-12-24 13:29:52 -0500

    replace argument list length checker with argparse

--- calibre_watchdog.py
+++ calibre_watchdog.py
@@ -1,13 +1,19 @@
-import time, subprocess, sys
+import time, subprocess, sys, os, argparse
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 def validateArgs():
-    if len(sys.argv) == 2:
-        return
+    parser = argparse.ArgumentParser(description = 'Auto-encode downloaded ePub files to UTF-8')
+    parser.add_argument('-p', '--path', required = True, dest = 'library_path', help = 'Path to Calibre library directory')
+    args = parser.parse_args()
+    library_path = args.library_path
+
+    if not os.path.exists(library_path):
+        sys.exit(f"{library_path} does not exist. Please double check your library path.")
+    elif not os.path.exists(library_path + "/metadata.db"):
+        sys.exit(f"Calibre library database file does not exists. Please double check your library path.")
     else:
-        print(USAGE)
-        sys.exit(0)
+        return library_path
 
 def checkCalibreDebugInstallation():
     calibreDebugInfo = subprocess.run(['which', 'calibre-debug'], capture_output=True)
@@ -17,10 +23,13 @@ def checkCalibreDebugInstallation():
         return True
 
 def on_created(event):
-    print(f"{event.src_path} has been created.")
-    subprocess.run(["calibre-debug", "/root/modify_epub_plugin/commandline/me.py", "--", "--help"])
+    print(f"\"{event.src_path}\" has been created. Calling Modify ePub CLI...")
+    subprocess.run(["calibre-debug", "/root/modify_epub_plugin/commandline/me.py", "--", event.src_path, "--encode_html_utf8"])
 
 def main():
+    checkCalibreDebugInstallation():
+    validateArgs()
+
     patterns = ["*.epub"]
     ignore_patterns = None
     ignore_directories = True
@@ -44,4 +53,4 @@ def main():
         my_observer.join()
 
 if __name__ == "__main__":
-    if checkCalibreDebugInstallation(): main()
+    main()
